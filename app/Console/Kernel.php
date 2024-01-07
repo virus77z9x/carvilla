be136bc3f9b04e2dc4506b84b8f1ed82efcc64d1
virus77z9x <virus77z9x@virus77z.virus77z>
2024-01-07 05:47:23 +0200

    =third modification

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,9 +10,13 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
+    protected $commands = [
+        \App\Console\Commands\expiration::class,
+    ] ;
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->command('user:expire')->everyMinute();
     }
 
     /**
